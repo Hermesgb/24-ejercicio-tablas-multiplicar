a52c772ffc7839a26346972c0a384ada044b427d
John Doe <johndoe@email.com>
2024-06-04 16:54:05 +0200

    Diagrama de flujo implementado.

--- tablas-multiplicar.js
+++ tablas-multiplicar.js
@@ -1,1 +1,17 @@
+function multiplicar(tabla, num) {
+    for (i = 0; i < 10; i++){
+        console.log("TABLA DEL " + tabla);
+        console.log("   ");
+        for (j = 0; j < 10;j++){
+            operacionMultiplicar = tabla * num;
+            console.log(tabla + " * " + num + " = " + operacionMultiplicar);
+            num++;
+        }
+        console.log("   ");
+        console.log("   ");
+        tabla++;
+        num = 1;
+    }
+}
 
+multiplicar(1, 1);
